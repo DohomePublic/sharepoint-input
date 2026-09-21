e28aae58fa5a1c685ea792c2def9c2515aa47c6f
DohomePublic <DohomePublic@dohome.co.th>
2026-09-21 15:11:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+window.KYC_CONFIG = {
+  clientId: 'a37bd62d-e74d-4ea0-9546-1eb5aa96f604',
+  tenantId: '7f8918d9-718a-495b-ac9a-17cba381c4a0',
+  sharePointHost: 'dohomegroup.sharepoint.com',
+  sitePath: '/sites/AC-Accounting',
+  listName: 'DemoApp',
+  scopes: ['User.Read','Sites.ReadWrite.All']
+};
